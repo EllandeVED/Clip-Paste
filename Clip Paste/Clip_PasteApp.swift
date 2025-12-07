a424008276fa01f5f9b410000ac4ea1d336059a8
EllandeVED <ellande.dascon@gmail.com>
2025-12-07 23:14:44 +0100

    Initial Commit

---
+++ Clip Paste/Clip_PasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clip_PasteApp.swift
+//  Clip Paste
+//
+//  Created by van Egmond Dascon on 07/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Clip_PasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clip PasteTests/Clip_PasteTests.swift
@@ -0,0 +1,17 @@
+//
+//  Clip_PasteTests.swift
+//  Clip PasteTests
+//
+//  Created by van Egmond Dascon on 07/12/2025.
+//
+
+import Testing
+@testable import Clip_Paste
+
+struct Clip_PasteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
